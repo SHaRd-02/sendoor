e1ba8cbd2e9284b5517f8a06bd4f00b22262a4a4
Ricardo <ricaroherniag@gmail.com>
2026-03-05 15:51:09 -0800

    initial commit

---
+++ api/app.py
@@ -0,0 +1,8 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("Door opened")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
