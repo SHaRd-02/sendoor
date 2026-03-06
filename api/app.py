c6672e01f7b0a9f595da645b31fede26653e8319
Ricardo <ricaroherniag@gmail.com>
2026-03-05 16:12:17 -0800

    feat: added post request

--- api/app.py
+++ api/app.py
@@ -1,8 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 
+door_status = "closed"
+
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print("Door opened")
+
+    def do_POST(self):
+        global door_status
+
+        door_status = "open"
+
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"door_open")
+        self.wfile.write(b"OK")
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(door_status.encode())
