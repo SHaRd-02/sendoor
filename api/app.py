e8ee3e2743ae8175e225fb3aba9327f1bb3ca1bc
Ricardo <ricaroherniag@gmail.com>
2026-03-05 16:29:38 -0800

    fix: status read

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
 door_status = "closed"
 
@@ -7,7 +8,15 @@ class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         global door_status
 
-        door_status = "open"
+        content_length = int(self.headers.get('Content-Length', 0))
+        body = self.rfile.read(content_length)
+
+        try:
+            data = json.loads(body.decode())
+            door_status = data.get("door", "closed")
+            print("Door status updated:", door_status)
+        except Exception as e:
+            print("Error parsing JSON:", e)
 
         self.send_response(200)
         self.end_headers()
